445da5a514f90be10fddc6529e9cbaf2be466274
JuanMaGD412 <juan.grandetd@udea.edu.co>
2025-04-14 18:53:45 -0500

    prueba vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,23 @@
+// db.js o lib/db.js
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: 'db.mepqgipnicweghojgycb.supabase.co',
-  user: 'postgres',
-  password: 'libroverde412',
-  database: 'postgres',
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 10,
-});
+let pool;
+
+if (!global.pgPool) {
+  global.pgPool = new Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    max: 10,
+  });
+}
+
+pool = global.pgPool;
 
 export default pool;
