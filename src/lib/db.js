0516724fe741a61ef53c5185c2bb92a93df91f46
JuanMaGD412 <juan.grandetd@udea.edu.co>
2025-06-12 22:07:50 -0500

    db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,11 @@
+// test-connection.js
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
   host: 'aws-0-us-east-1.pooler.supabase.com',
   user: 'postgres.mepqgipnicweghojgycb',
-  password: 'proyectos1234',
+  password: 'ProjectsJuanma412',
   database: 'postgres',
   port: 5432,
   ssl: {
